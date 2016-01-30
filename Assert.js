f7f8193191fd3805a1adcd8232c7247c9fe5134b
sqny <sqnyster@gmail.com>
2016-01-30 23:01:35 +0100

    2

---
+++ Assert.js
@@ -0,0 +1,24 @@
+(function () {
+
+    this.assert = function (value,description) {
+        setTimeout(function () {
+            var li, result;
+            result = document.getElementById('result');
+            if (!result) {
+
+                result = document.createElement("ul");
+                result.id = 'result';
+                document.body.appendChild(result);
+            }
+            result.style.background = '#FFFFA0';
+            result.style.border = "thick solid red";
+
+            li = document.createElement("li");
+            li.textContent = description;
+            li.style.color = value ? 'green' : 'red';
+            if (!value)li.style.textDecoration = 'line-through';
+            result.appendChild(li);
+        }, 100);
+    }
+
+})();
